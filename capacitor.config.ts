49639941e05757e181ade95bdd40f8489cf0ef91
sinfoniavirale-crypto <sinfoniavirale@gmail.com>
2026-09-07 10:40:48 +0200

    Add files via upload

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.mitologiagreca.app',
+  appName: 'Mitologia Greca',
+  webDir: 'dist'
+};
+
+export default config;
